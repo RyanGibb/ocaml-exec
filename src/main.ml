075ce323b0449f8e7ef423c66cb6b35a9b4f4422
Ryan Gibb <ryan@freumh.org>
2023-03-10 17:02:54 +0000

    use eio to proxy std in/out to/from pipe

--- src/main.ml
+++ src/main.ml
@@ -1,32 +1,34 @@
 
 let () =
+  Eio_main.run @@ fun env ->
   let readInpipe, writeInpipe = Unix.pipe () in
   let readOutpipe, writeOutpipe = Unix.pipe () in
   (* fork–exec *)
   let pid = Unix.fork () in
-  (* If parent *)
+  (* if parent *)
   if pid != 0 then (
   (* close unused pipes *)
     Unix.close writeInpipe;
     Unix.close readOutpipe;
+    let close_unix = true in
     let read, write = readInpipe, writeOutpipe in
-    (* Unix.dup2 Unix.stdin read;
-    Unix.dup2 Unix.stdout write; *)
-    let _ = Unix.write_substring write "echo hi\n" 0 8 in
-    let buf = Bytes.create 100 in
-    let len = Unix.read read buf 0 100 in
-    let s = Bytes.sub_string buf 0 len in
-    print_endline s;
-    let _pid, _status = Unix.wait () in
-    ();
+      Eio.Fiber.both
+        (fun () -> Eio.Switch.run @@ fun sw ->
+          let sink = Eio_unix.FD.as_socket ~sw ~close_unix write in
+          Eio.Flow.copy (Eio.Stdenv.stdin env) sink)
+        (fun () -> Eio.Switch.run @@ fun sw ->
+          let source = Eio_unix.FD.as_socket ~sw ~close_unix read in
+          Eio.Flow.copy source (Eio.Stdenv.stdout env))
   ) else
     (* close unused pipes *)
     Unix.close writeOutpipe;
     Unix.close readInpipe;
     let read, write = readOutpipe, writeInpipe in
+    (* redirect stdin stdout to and from pipe *)
     Unix.dup2 read Unix.stdin;
     Unix.dup2 write Unix.stdout;
-    (* redirect stdin stdout to and from pipe *)
-    (* close unused pipes *)
-    (* redirect stdin to and from pipe *)
-    Unix.execve "/run/current-system/sw/bin/bash" [| |] [| |]
+    Unix.execve "/run/current-system/sw/bin/bash"
+      (* login shell *)
+      [| "-bash"; |]
+      (Unix.unsafe_environment ());;
+      (* [| "PATH=" ^ Unix.getenv "PATH" |];; *)
