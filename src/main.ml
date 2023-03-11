a67004b67ccb934d93e363188956b34e5097b2a3
Ryan Gibb <ryan@freumh.org>
2023-03-11 18:35:26 +0000

    created pty for interactive shell

--- src/main.ml
+++ src/main.ml
@@ -1,34 +1,30 @@
-
 let () =
   Eio_main.run @@ fun env ->
-  let readInpipe, writeInpipe = Unix.pipe () in
-  let readOutpipe, writeOutpipe = Unix.pipe () in
+  let pty = Pty.open_pty () in
   (* fork–exec *)
   let pid = Unix.fork () in
   (* if parent *)
   if pid != 0 then (
-  (* close unused pipes *)
-    Unix.close writeInpipe;
-    Unix.close readOutpipe;
     let close_unix = true in
-    let read, write = readInpipe, writeOutpipe in
-      Eio.Fiber.both
-        (fun () -> Eio.Switch.run @@ fun sw ->
-          let sink = Eio_unix.FD.as_socket ~sw ~close_unix write in
-          Eio.Flow.copy (Eio.Stdenv.stdin env) sink)
-        (fun () -> Eio.Switch.run @@ fun sw ->
-          let source = Eio_unix.FD.as_socket ~sw ~close_unix read in
-          Eio.Flow.copy source (Eio.Stdenv.stdout env))
+    Eio.Fiber.both
+      (fun () -> Eio.Switch.run @@ fun sw ->
+        let sink = Eio_unix.FD.as_socket ~sw ~close_unix pty.Pty.masterfd in
+        Eio.Flow.copy (Eio.Stdenv.stdin env) sink)
+      (fun () -> Eio.Switch.run @@ fun sw ->
+        let source = Eio_unix.FD.as_socket ~sw ~close_unix pty.Pty.masterfd in
+        Eio.Flow.copy source (Eio.Stdenv.stdout env))
   ) else
-    (* close unused pipes *)
-    Unix.close writeOutpipe;
-    Unix.close readInpipe;
-    let read, write = readOutpipe, writeInpipe in
-    (* redirect stdin stdout to and from pipe *)
-    Unix.dup2 read Unix.stdin;
-    Unix.dup2 write Unix.stdout;
-    Unix.execve "/run/current-system/sw/bin/bash"
+    Unix.close pty.Pty.masterfd;
+    Pty.switch_controlling_pty pty;
+    (* Pty.window_size pty pty_window; *)
+    Unix.dup2 pty.Pty.slavefd Unix.stdin;
+    Unix.dup2 pty.Pty.slavefd Unix.stdout;
+    Unix.dup2 pty.Pty.slavefd Unix.stderr;
+    Unix.close pty.Pty.slavefd;
+    try Unix.execve "/run/current-system/sw/bin/bash"
       (* login shell *)
       [| "-bash"; |]
-      (Unix.unsafe_environment ());;
+      (Unix.unsafe_environment ())
       (* [| "PATH=" ^ Unix.getenv "PATH" |];; *)
+    with Unix.Unix_error (x,_s,y) ->
+      print_endline (Printf.sprintf "%s: %s" y (Unix.error_message x));;
