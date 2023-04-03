868ab06b2377bcedbe4093eeb5a0efd782bf4a2c
Ryan Gibb <ryan@freumh.org>
2023-04-03 14:48:21 +0100

    simplify

--- src/main.ml
+++ src/main.ml
@@ -1,11 +1,6 @@
 
-let client ~stdout ~stdin ~write ~read =
-  Eio.Fiber.both
-    (fun () -> Eio.Flow.copy stdin write)
-    (fun () -> Eio.Flow.copy read stdout)
-
 let () =
-  Eio_main.run @@ fun env ->
+  Eio_main.run @@ fun _env ->
   Eio.Switch.run @@ fun sw ->
   (* Could use Eio_unix.pipe with higher level Eio process lib
      but need Eio_linux.FD for Eio_linux.Low_level.Process.spawn *)
@@ -15,11 +10,4 @@ let () =
       ~argv:[| "-bash" |]
       ~env:[||] in
     Eio_linux.Low_level.Process.spawn ~sw [ action ] in
-  let readInpipe, _writeInpipe = Eio_unix.pipe sw in
-  let _readOutpipe, writeOutpipe = Eio_unix.pipe sw in
-  client
-    ~stdout:(Eio.Stdenv.stdout env)
-    ~stdin:(Eio.Stdenv.stdin env)
-    ~write:writeOutpipe
-    ~read:readInpipe;
   ignore @@ Eio.Promise.await (Eio_linux.Low_level.Process.exit_status child);;
